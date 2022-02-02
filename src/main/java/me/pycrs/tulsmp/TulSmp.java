986f50469a8eed9327c837a2e0f063b5ffe20ab7
Petr (Desktop) <petrbohac3@seznam.cz>
2022-02-02 16:29:56 +0100

    Initial commit

---
+++ src/main/java/me/pycrs/tulsmp/TulSmp.java
@@ -0,0 +1,17 @@
+package me.pycrs.tulsmp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TulSmp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
